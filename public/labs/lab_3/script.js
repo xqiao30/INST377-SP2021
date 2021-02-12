9d4fcf1b4e471b8ccaa3f7dde678210e86f723de
xqiao30 <78039384+xqiao30@users.noreply.github.com>
2021-02-12 17:02:02 +0800

    lab3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,60 +1,24 @@
-function arraymethod() {
-  const rightButton = doucment.querySelector('scrollright');
-  const leftButton = document.querySelector('scrollLeft');
-  rightButton.addEventListener('click', (event) => {
-    //const array1 = [1, 2, 3, 4, 5]; 
-    const layout = document.querySelector('layout1');
-    const classes = layout.classList;
-    console.log(classes.value);
-    let track = 1;
-    let className;
-    if (track !== 5) {
-      if (classes.value === 'layout1') {
-        className = 'layout2';
-        track = 2;
-      }
-      else if (classes.value === 'layout2') {
-        className = 'layout3';
-        track = 3;
-      }
-      else if (classes.value === 'layout3') {
-        className = 'layout4';
-        track = 4;
-      }
-      else if (classes.value === 'layout4') {
-        className = 'layout5';
-        track = 5;
-      }
-    }
-    console.log(className);
-    layout.classlist = className;
-  })
-  leftButton.addEventListener('click', (event) => {
-    const layout = document.querySelector('layout1');
-    const classes = layout.classList;
-    console.log(classes.value);
-    let track = 5;
-    let className;
-    if (track !== 1) {
-      if (classes.value === 'layout2') {
-        className = 'layout1';
-        track = 1;
-      }
-      else if (classes.value === 'layout3') {
-        className = 'layout2';
-        track = 2;
-      }
-      else if (classes.value === 'layout4') {
-        className = 'layout3';
-        track = 3;
-      }
-      else if (classes.value === 'layout5') {
-        className = 'layout4';
-        track = 4;
-      }
-    }
-    console.log(className);
-    layout.classlist = className;
-})
+const images = ['images/onigiri_1.png', 'images/onigiri_2.png', 'images/onigiri_3.png', 'images/onigiri_4.png', 'images/roll_1.png', 'images/roll_2.png', 'images/roll_3.png'];
+let index = 0;
+let the_image = document.getElementById("main-image");
+the_image.src = images[0];
+
+function show_image(direction)
+{
+  if (direction == "left")
+  {
+    index--;
+  }
+  else
+  {
+    index++;
+    index %= images.length;
+  }
+  
+  if (index < 0)
+  {
+    index = images.length - 1;
+  }
+  
+  the_image.src = images[index];
 }
-window.onload = arraymethod;
