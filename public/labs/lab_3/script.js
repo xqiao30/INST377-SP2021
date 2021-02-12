15de52ac572f94b45919e7c91856b14989b6eff3
xqiao30 <78039384+xqiao30@users.noreply.github.com>
2021-02-12 15:47:19 +0800

    lab3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,12 +1,60 @@
-/* Put your javascript in here */
 function arraymethod() {
-const array1 = [1, 2, 3, 4, 5];
-const count = array1[0];
-const imageLayout = document.querySelector('layout1');
-const classes = layout1.classlist;
-console.log(classes.value);
-imageLayout.addEventListener('click', (event) =>{
-
+  const rightButton = doucment.querySelector('scrollright');
+  const leftButton = document.querySelector('scrollLeft');
+  rightButton.addEventListener('click', (event) => {
+    //const array1 = [1, 2, 3, 4, 5]; 
+    const layout = document.querySelector('layout1');
+    const classes = layout.classList;
+    console.log(classes.value);
+    let track = 1;
+    let className;
+    if (track !== 5) {
+      if (classes.value === 'layout1') {
+        className = 'layout2';
+        track = 2;
+      }
+      else if (classes.value === 'layout2') {
+        className = 'layout3';
+        track = 3;
+      }
+      else if (classes.value === 'layout3') {
+        className = 'layout4';
+        track = 4;
+      }
+      else if (classes.value === 'layout4') {
+        className = 'layout5';
+        track = 5;
+      }
+    }
+    console.log(className);
+    layout.classlist = className;
+  })
+  leftButton.addEventListener('click', (event) => {
+    const layout = document.querySelector('layout1');
+    const classes = layout.classList;
+    console.log(classes.value);
+    let track = 5;
+    let className;
+    if (track !== 1) {
+      if (classes.value === 'layout2') {
+        className = 'layout1';
+        track = 1;
+      }
+      else if (classes.value === 'layout3') {
+        className = 'layout2';
+        track = 2;
+      }
+      else if (classes.value === 'layout4') {
+        className = 'layout3';
+        track = 3;
+      }
+      else if (classes.value === 'layout5') {
+        className = 'layout4';
+        track = 4;
+      }
+    }
+    console.log(className);
+    layout.classlist = className;
 })
 }
 window.onload = arraymethod;
