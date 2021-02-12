a39e1fbf7a5c9348e4e77ac44b4bcbba2a542c89
xqiao30 <78039384+xqiao30@users.noreply.github.com>
2021-02-12 13:28:39 +0800

    lab3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,8 @@
 /* Put your javascript in here */
+function arraymethod() {
+const array1 = [1,2,3,4,5];
+const imageLayout = document.querySelector('#box1');
+imageLayout.addEventListener('click', (event) =>{
+
+})
+}
