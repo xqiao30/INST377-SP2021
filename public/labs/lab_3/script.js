807a4871b4a08a235be2d060dc23ffec71221408
xqiao30 <78039384+xqiao30@users.noreply.github.com>
2021-02-12 14:49:10 +0800

    lab3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,8 +1,12 @@
 /* Put your javascript in here */
 function arraymethod() {
-const array1 = [1,2,3,4,5];
-const imageLayout = document.querySelector('#box1');
+const array1 = [1, 2, 3, 4, 5];
+const count = array1[0];
+const imageLayout = document.querySelector('layout1');
+const classes = layout1.classlist;
+console.log(classes.value);
 imageLayout.addEventListener('click', (event) =>{
 
 })
 }
+window.onload = arraymethod;
