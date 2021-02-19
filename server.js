3a99c8f9d1296fc842167ac23c98dce146bd2421
xqiao30 <78039384+xqiao30@users.noreply.github.com>
2021-02-19 14:33:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.route('/api')
     res.json(json);
   })
   .post(async (req, res) => {
-    console.log('POST request detected');
+    console.log('Hello World');
     console.log('Form data in res.body', req.body);
     console.log('Now send something back to your client');
     // res.json({data: dataToSendToFrontEnd});
